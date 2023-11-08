54f5a9b86be7da2be2f956cdfac15cc54f8526f3
Дульский Максим Иванович <pr130dulskiy@oat.local>
2023-11-08 17:39:50 +0600

    day 3

---
+++ app/src/main/java/com/example/ppactic/Creating_code.kt
@@ -0,0 +1,11 @@
+package com.example.ppactic
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Creating_code : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.creating_code)
+    }
+}
